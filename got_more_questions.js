cc0d6db77d92cd64b871a5d6beaf72e270ef2741
usman <kohistani5525@gmail.com>
2023-06-09 01:32:24 +0500

    got more questions a lilbit difficult

---
+++ got_more_questions.js
@@ -0,0 +1,43 @@
+const users = [
+    { id: 1, name: "John", age: 25, score: 80, hobbies: ["Reading", "Soccer"], address: { city: "New York", country: "USA" } },
+    { id: 2, name: "Sarah", age: 32, score: 90, hobbies: ["Painting", "Cooking"], address: { city: "London", country: "UK" } },
+    { id: 3, name: "Michael", age: 28, score: 70, hobbies: ["Gardening", "Photography"], address: { city: "Sydney", country: "Australia" } },
+    { id: 4, name: "Emily", age: 21, score: 85, hobbies: ["Dancing", "Hiking"], address: { city: "Paris", country: "France" } },
+    { id: 5, name: "David", age: 35, score: 95, hobbies: ["Guitar", "Traveling"], address: { city: "Tokyo", country: "Japan" } },
+    { id: 6, name: "Jessica", age: 29, score: 75, hobbies: ["Yoga", "Movies"], address: { city: "Berlin", country: "Germany" } },
+    { id: 7, name: "Andrew", age: 31, score: 88, hobbies: ["Swimming", "Chess"], address: { city: "Toronto", country: "Canada" } },
+    { id: 8, name: "Sophia", age: 27, score: 92, hobbies: ["Painting", "Cooking"], address: { city: "Rome", country: "Italy" } },
+    { id: 9, name: "Daniel", age: 24, score: 83, hobbies: ["Photography", "Hiking"], address: { city: "Cairo", country: "Egypt" } },
+    { id: 10, name: "Olivia", age: 30, score: 87, hobbies: ["Reading", "Soccer"], address: { city: "Rio de Janeiro", country: "Brazil" } },
+];
+
+// What is the name of the user with the highest score?
+//     What is the city of residence for Sarah?
+//                                           How many hobbies does John have?
+//     Which user has the lowest score?
+//     How many users are from countries outside of Europe?
+//     What is the average age of all users?
+//     Which user has the longest name?
+//     How many users have a score greater than 80?
+//     Which user is the oldest?
+//     How many users have the hobby of "Cooking"?
+//     What is the country of residence for the user with the lowest score?
+//     How many users have an age between 25 and 30 (inclusive)?
+//     Which user has the highest score and lives in Australia?
+//     How many users have the hobby of "Reading" or "Soccer"?
+//     Which user has the shortest name?
+//     How many users are from cities with a name longer than 5 characters?
+//     What is the average score of users from Europe?
+//     How many users have an age greater than their score?
+//     Which user has the highest score and lives in a city with a name longer than 7 characters?
+//     How many users have more than one hobby?
+//     Which user has the highest score among users under the age of 25?
+//     How many users have a name that starts with the letter "J"?
+//     Which user has the highest score and lives in a country starting with the letter "U"?
+//     How many users have an age that is a multiple of 3?
+//     Which user has the highest score and lives in a city that starts with the letter "R"?
+//     How many users have a name longer than 6 characters and an age less than 30?
+//     Which user has the lowest score among users over the age of 30?
+//     How many users have a hobby that contains the word "Gardening"?
+//     Which user has the highest combined length of their hobbies?
+//     How many users have a score that is a prime number?
