96975d73269fce524f5f5fd826ff4bcb7e08c51d
usman <kohistani5525@gmail.com>
2023-06-13 01:18:39 +0500

    5 of 20 Questions

--- writeSomeFunctions.js
+++ writeSomeFunctions.js
@@ -1,9 +1,39 @@
 // day-3 : JS Practice .
 // 1. Write a function to capitalize the first letter of each word in a given string.
+let str = 'muhammad Usman is my name'
+const string = 'Usman'
+function capitalize(str){
+   const upperCase = str.split(' ')
+    return upperCase.map(x=>x.charAt(0).toUpperCase()+x.slice(1)).join(' ')
+}
+const a = capitalize(str)
+// console.log(a)
+
 // 2. Implement a function that checks if a given string is a palindrome.
+const isPalindrome = (str) => {
+    const reversedStr = str.split('').reverse().join('')
+    if (str === reversedStr) console.log('yes the string is palindrome: ',str); else console.log("no it's not a palindrome")
+}
+isPalindrome(string)
 // 3. Write a function that counts the occurrences of each element in an array and returns the count as an object.
+const words = ['usman','pakistan','pakistan','usman','hello world']
+const dictionary = (words) => {
+    const obj = {}
+    words.map(x=> {
+       obj[x] =  (obj[x] || 0) + 1  //obj[x] means a key : if a key does not exist add 0 as its value and if it exists add 1 to its value
+    }
+    )
+    return obj
+}
+console.log(dictionary(words))
 // 4. Create a function that calculates the factorial of a given number.
+const factorial = (n) => {
+    if (n ===0 || n === 1 ) return 1;else return n * factorial(n-1)
+}
+console.log(factorial(5))
 // 5. Implement a function that removes duplicates from an array.
+const removeDuplicate = (str) => Array.from(new Set([...str]))
+console.log(removeDuplicate(words))
 // 6. Write a function that flattens a nested array into a single level array.
 // 7. Create a function to find the maximum and minimum values in an array.
 // 8. Implement a function that checks if two arrays are equal (have the same elements in the same order).
