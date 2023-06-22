8c9fba9fa7d1dcaa4569db6c14181715aa042c98
usman <kohistani5525@gmail.com>
2023-06-22 23:58:41 +0500

    to be consistant

--- writeSomeFunctions.js
+++ writeSomeFunctions.js
@@ -101,7 +101,47 @@ const mostFrequent = (arr) => {
 
 console.log(mostFrequent(['hello', 'hello', 'gee', 'one']))
 // 13. Create a function that converts a number to its corresponding Roman numeral.
+const convertToRoman = (num) => {
+  const romanNumerals = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ]
+
+  let result = ''
+
+  for (const { value, numeral } of romanNumerals) {
+    while (num >= value) {
+      result += numeral
+      num -= value
+    }
+  }
+
+  return result
+}
+
+console.log(convertToRoman(2023))
+
+
 // 14. Implement a function that removes falsy values (false, null, 0, "", undefined, and NaN) from an array.
+const removeFalsyValues = (arr) => arr.filter(Boolean);
+
+const array = [false, null, 0, "", undefined, NaN, "Hello", 42];
+const filteredArray = removeFalsyValues(array);
+
+console.log(filteredArray);
+
+
 // 15. Write a function that returns the sum of all numbers in an array.
 // 16. Create a function that finds the longest word in a sentence.
 // 17. Implement a function that checks if two strings are anagrams.
